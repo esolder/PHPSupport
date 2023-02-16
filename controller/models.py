9de52acc80cd9567b950e0f1675db7fb0b51f45b
Esolder <40249820+Esolder@users.noreply.github.com>
2023-02-16 12:24:43 +0300

    created models

--- controller/models.py
+++ controller/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
+from django.utils import timezone
 
-# Create your models here.
+class Client(models.Model):
+    username = models.CharField('Уникальное имя пользователя telegram', max_length=32)
+    subscription_end = models.DateField()
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+    
+
+class Executor(models.Model):
+    username = models.CharField('Уникальное имя пользователя telegram', max_length=32)
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'Подрядчик'
+        verbose_name_plural = 'Подрядчики'
+
+
+class Rate(models.Model):
+    rate = models.DecimalField('Ставка за заказ в рублях', max_digits=10, decimal_places=2)
+    when_set = models.DateTimeField('Когда установлена ставка', auto_now_add=True)
+
+    def __str__(self):
+        return f'Ставка: {self.rate} р. Установлена: {self.when_set.date()}'
+    
+    class Meta:
+        verbose_name = 'Ставка за заказ'
+        verbose_name_plural = 'Ставки за заказы'
+        ordering = ['-when_set']
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, 
+                               on_delete=models.CASCADE,
+                               related_name='orders',
+                               verbose_name='Клиент')
+    executor = models.ForeignKey(Executor, 
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True,
+                                 related_name='orders',
+                                 verbose_name='Подрядчик')
+    is_booked = models.BooleanField('Заказ забронирован', default=False)
+    is_taken = models.BooleanField('Заказ взят',
+                                   default=False)
+    is_complete = models.BooleanField('Заказ выполнен',
+                                      default=False)
+    rate = models.DecimalField('Ставка в рублях',
+                               max_digits=10,
+                               decimal_places=2,
+                               blank=True)
+    estimate = models.CharField('Оценка времени выполнения заказа',
+                                max_length=100,
+                                blank=True,
+                                null=True)
+    complete_date = models.DateField('Дата закрытия заказа',
+                                     blank=True,
+                                     null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            latest_rate = Rate.objects.latest('when_set')
+            self.rate = latest_rate.rate
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Заказ {self.id} от {self.client}'
+    
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+@receiver(pre_save, sender=Order)
+def set_complete_date(sender, instance, **kwqrgs):
+    if instance.is_complete:
+        instance.complete_date = timezone.now().date()
+
+
+@receiver(pre_delete, sender=Executor)
+def executor_delete_handler(sender, instance, **kwargs):
+    orders = instance.orders.filter(is_taken=True)
+    orders.update(is_taken=False, estimate='', is_booked=False)
