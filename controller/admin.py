9aad6e780d8abeb8f988f9fe50775d7c690b4990
Esolder <40249820+Esolder@users.noreply.github.com>
2023-02-16 12:25:36 +0300

    registered models in admin panel

--- controller/admin.py
+++ controller/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Executor, Order, Rate
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('username', 'subscription_end')
+
+
+@admin.register(Executor)
+class ExecutorAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('executor',
+                       'is_booked',
+                       'rate',
+                       'is_taken',
+                       'is_complete',
+                       'estimate',
+                       'complete_date',)
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    ...
